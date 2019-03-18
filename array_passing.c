18033511b3530fd40128f799d2d270abdf8a3ed9
Yatheesha Sunnara <yatheesha.sunnara.k@gmail.com>
2019-03-18 19:33:15 +0530

    Initial commit

--- array_passing.c
+++ array_passing.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define row 3
 #define column 4
 void display(int *arr, int m, int n)
@@ -13,6 +14,22 @@ void display(int *arr, int m, int n)
         }
         printf("\n");
     }
+}
+
+void displaychar(char *ch)
+{
+    printf("%s\t", ch);
+}
+
+void modifycharpointer(char **ch)
+{
+    *ch = "Hello";
+}
+
+void modifychar(char *ch)
+{
+    strcpy(ch, "Hello");
+    // *ch = "Hello\n";
 }
 
 void modify(int *arr, int m, int n)
@@ -33,16 +50,31 @@ int main()
                             {3, 4, 5, 6},
                             {6, 7, 8, 9}};
     int arr2[column] = {0, 1, 2, 3};
+    char *c = "Hai", ch[] = "Good";
     printf("Before size of array: %d\n", (int)(sizeof(arr) / sizeof(int)));
     display((int *)arr, row, column);
     modify((int *)arr, row, column);
     printf("After\n");
     display((int *)arr, row, column);
 
-    printf("Before\n");
+    printf("Before size of array: %d\n", (int)(sizeof(arr2) / sizeof(int)));
     display((int *)arr2, 1, column);
     modify((int *)arr2, 1, column);
     printf("After\n");
     display((int *)arr2, 1, column);
+
+    printf("Before size of array: %d\n", (int)(sizeof(c) / sizeof(char)));
+    displaychar(c);
+    modifycharpointer(&c);
+    printf("\nAfter\n");
+    displaychar(c);
+    printf("\n");
+
+    printf("Before size of array: %d\n", (int)(sizeof(ch) / sizeof(char)));
+    displaychar(ch);
+    modifychar(ch);
+    printf("\nAfter\n");
+    displaychar(ch);
+    printf("\n");
     return 0;
 }
